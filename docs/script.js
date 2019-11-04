9ac024ed9a7190a7c1eb4cf1b0286c0a0b82ee18
METaylor05 <Malcolmthedev@gmail.com>
2019-11-04 15:00:15 -0500

    month1 files

---
+++ docs/script.js
@@ -0,0 +1,68 @@
+var slidesArray = [];
+var slideNumber = 0;
+
+var mainSlide0 = document.querySelector(".slide-1");
+var mainSlide1 = document.querySelector(".slide-2");
+var mainSlide2 = document.querySelector(".slide-3");
+slidesArray[0] = mainSlide0;
+slidesArray[1] = mainSlide1;
+slidesArray[2] = mainSlide2;
+
+
+
+/************************************Function to move slide forward*********************************/ 
+function slideNext () 
+{
+    console.log(slidesArray);
+    
+    if (slideNumber == 0)
+        {
+            slideNumber = slideNumber + 1;
+            mainSlide0.classList.toggle("show");
+            mainSlide1.classList.toggle("show");
+            console.log(slidesArray);
+            
+        }
+    else if (slideNumber == 1)
+        {
+            slideNumber = slideNumber + 1;
+            mainSlide1.classList.toggle("show");
+            mainSlide2.classList.toggle("show");
+        }
+    
+    else if (slideNumber == 2)
+        {
+            slideNumber = 0;
+            mainSlide2.classList.toggle("show");
+            mainSlide0.classList.toggle("show");
+        }
+    
+    
+}
+
+/************************************Function to move slide Backward*********************************/ 
+function slideBefore ()
+{
+    if (slideNumber == 2)
+        {
+            slideNumber = slideNumber - 1;
+            mainSlide2.classList.toggle("show");
+            mainSlide1.classList.toggle("show");
+            
+        }
+    
+    else if (slideNumber == 1)
+        {
+            slideNumber = slideNumber - 1;
+            mainSlide1.classList.toggle("show");
+            mainSlide0.classList.toggle("show");
+        }
+    
+    else if (slideNumber == 0)
+        {
+            slideNumber = 2;
+            mainSlide0.classList.toggle("show");
+            mainSlide2.classList.toggle("show");
+        }
+}
+
